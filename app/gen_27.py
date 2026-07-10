b52a7c34eaa641e2644cb72140778359383396f5
campaign <campaign@thesis.local>
2026-07-10 17:30:47 +0100

    [035/risky] feat: large module gen_27 (light tests)

---
+++ app/gen_27.py
@@ -0,0 +1,109 @@
+"""Generated module 27 (campaign)."""
+
+
+def value_27(x):
+    """Return x scaled by 8 plus 5."""
+    return x * 8 + 5
+
+
+def helper_27_0(x):
+    return x + 0
+
+def helper_27_1(x):
+    return x + 1
+
+def helper_27_2(x):
+    return x + 2
+
+def helper_27_3(x):
+    return x + 3
+
+def helper_27_4(x):
+    return x + 4
+
+def helper_27_5(x):
+    return x + 5
+
+def helper_27_6(x):
+    return x + 6
+
+def helper_27_7(x):
+    return x + 7
+
+def helper_27_8(x):
+    return x + 8
+
+def helper_27_9(x):
+    return x + 9
+
+def helper_27_10(x):
+    return x + 10
+
+def helper_27_11(x):
+    return x + 11
+
+def helper_27_12(x):
+    return x + 12
+
+def helper_27_13(x):
+    return x + 13
+
+def helper_27_14(x):
+    return x + 14
+
+def helper_27_15(x):
+    return x + 15
+
+def helper_27_16(x):
+    return x + 16
+
+def helper_27_17(x):
+    return x + 17
+
+def helper_27_18(x):
+    return x + 18
+
+def helper_27_19(x):
+    return x + 19
+
+def helper_27_20(x):
+    return x + 20
+
+def helper_27_21(x):
+    return x + 21
+
+def helper_27_22(x):
+    return x + 22
+
+def helper_27_23(x):
+    return x + 23
+
+def helper_27_24(x):
+    return x + 24
+
+def helper_27_25(x):
+    return x + 25
+
+def helper_27_26(x):
+    return x + 26
+
+def helper_27_27(x):
+    return x + 27
+
+def helper_27_28(x):
+    return x + 28
+
+def helper_27_29(x):
+    return x + 29
+
+def helper_27_30(x):
+    return x + 30
+
+def helper_27_31(x):
+    return x + 31
+
+def helper_27_32(x):
+    return x + 32
+
+def helper_27_33(x):
+    return x + 33
