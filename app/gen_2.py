8b36fd9b656aa9e601e22cbf269e161e7eba398c
campaign <campaign@thesis.local>
2026-07-10 16:51:00 +0100

    [002/risky] feat: large module gen_2 (light tests)

---
+++ app/gen_2.py
@@ -0,0 +1,139 @@
+"""Generated module 2 (campaign)."""
+
+
+def value_2(x):
+    """Return x scaled by 5 plus 5."""
+    return x * 5 + 5
+
+
+def helper_2_0(x):
+    return x + 0
+
+def helper_2_1(x):
+    return x + 1
+
+def helper_2_2(x):
+    return x + 2
+
+def helper_2_3(x):
+    return x + 3
+
+def helper_2_4(x):
+    return x + 4
+
+def helper_2_5(x):
+    return x + 5
+
+def helper_2_6(x):
+    return x + 6
+
+def helper_2_7(x):
+    return x + 7
+
+def helper_2_8(x):
+    return x + 8
+
+def helper_2_9(x):
+    return x + 9
+
+def helper_2_10(x):
+    return x + 10
+
+def helper_2_11(x):
+    return x + 11
+
+def helper_2_12(x):
+    return x + 12
+
+def helper_2_13(x):
+    return x + 13
+
+def helper_2_14(x):
+    return x + 14
+
+def helper_2_15(x):
+    return x + 15
+
+def helper_2_16(x):
+    return x + 16
+
+def helper_2_17(x):
+    return x + 17
+
+def helper_2_18(x):
+    return x + 18
+
+def helper_2_19(x):
+    return x + 19
+
+def helper_2_20(x):
+    return x + 20
+
+def helper_2_21(x):
+    return x + 21
+
+def helper_2_22(x):
+    return x + 22
+
+def helper_2_23(x):
+    return x + 23
+
+def helper_2_24(x):
+    return x + 24
+
+def helper_2_25(x):
+    return x + 25
+
+def helper_2_26(x):
+    return x + 26
+
+def helper_2_27(x):
+    return x + 27
+
+def helper_2_28(x):
+    return x + 28
+
+def helper_2_29(x):
+    return x + 29
+
+def helper_2_30(x):
+    return x + 30
+
+def helper_2_31(x):
+    return x + 31
+
+def helper_2_32(x):
+    return x + 32
+
+def helper_2_33(x):
+    return x + 33
+
+def helper_2_34(x):
+    return x + 34
+
+def helper_2_35(x):
+    return x + 35
+
+def helper_2_36(x):
+    return x + 36
+
+def helper_2_37(x):
+    return x + 37
+
+def helper_2_38(x):
+    return x + 38
+
+def helper_2_39(x):
+    return x + 39
+
+def helper_2_40(x):
+    return x + 40
+
+def helper_2_41(x):
+    return x + 41
+
+def helper_2_42(x):
+    return x + 42
+
+def helper_2_43(x):
+    return x + 43
